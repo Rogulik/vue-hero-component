52f51deb8fdb7a043891f6c10ed280679b603a32
Marcin Rygielski <mrogulik@gmail.com>
2021-11-21 23:54:45 +0100

    emit and responsivnes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
-  <Hero imageURL="https://www.stevenstone.co.uk/website_steven_stone/static/src/images/home/chrismas.png" leadText="discover" titleText="your stone" descriptionText="Start your engagement ring journey here. Craft your own bespoke design or browse from our ready to wear collection." :buttonAVisible="true" buttonABgc="" buttonATextC="#fff" buttonAText="new arrivals"  :buttonBVisible="true" buttonBBgc="" buttonBTextC="" buttonBText="best sellers"  />
+  <Hero imageURL="https://www.stevenstone.co.uk/website_steven_stone/static/src/images/home/chrismas.png" leadText="discover" titleText="your stone" descriptionText="Start your engagement ring journey here. Craft your own bespoke design or browse from our ready to wear collection." :buttonAVisible="isButtonAVisible"
+  buttonABgc=""
+  buttonATextC="#fff"
+  buttonAText="new arrivals"
+  @buttonAEvent="onButtonAEvent"
+  :buttonBVisible="isButtonBVisible"
+  buttonBBgc="" 
+  buttonBTextC="" 
+  buttonBText="best sellers"
+  @buttonBEvent="onButtonBEvent" />
 </template>
 
 <script lang="ts">
@@ -8,10 +17,22 @@ import Hero from './components/Hero.vue'
 
 export default defineComponent({
   name: 'App',
+  data(){
+    return {
+      isButtonAVisible: true,
+      isButtonBVisible: true
+    }
+  },
   components: {
     Hero
   },
   methods:{
+    onButtonAEvent(value:string){
+      console.log(value)
+    },
+     onButtonBEvent(value:string){
+      console.log(value)
+    }
   }
 });
 </script>
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,14 +1,15 @@
 <template>
     <div class="w-full h-[400px] bg-hero-pattern bg-no-repeat bg-cover font-senkron" :style='`background-image: url(${imageURL})`'>
-    <div class="pt-[64px] ml-[115px] ">
+    <div class="pt-[84px] p-10 md:ml-[115px] md:pt-[64px]">
       <header class="text-white w-[432px]">
         <h2 class="font-light text-[28px] mb-[-13px] uppercase">{{leadText}}</h2>
         <h1 class="font-bold leading-[75px] text-[50px] uppercase">{{titleText}}</h1>
-        <p class="text-[18px] leading-[24px]">{{descriptionText}}</p>
+        <p class=" md:block  hidden text-[18px] leading-[24px]">{{descriptionText}}</p>
     </header>
-    <div class="flex justify-between font-nunito text-white w-[385px] mt-[32px]">
-      <button :style="{backgroundColor: buttonABgc, color: buttonATextC}" :class='`btn-main ${buttonAVisible ? "visible" : "invisible"}`'>{{buttonAText}}</button>
-      <button :style="{backgroundColor: buttonBBgc, color: buttonBTextC}" :class='`btn-sub  ${buttonBVisible ? "visible" : "invisible"}`'>{{buttonBText}}</button>
+    <div class="flex flex-col items-center md:flex-row  justify-between font-nunito text-white md:w-[385px] mt-[32px]">
+      <button @click="emitButtonA" :style="{backgroundColor: buttonABgc, color: buttonATextC}" :class='`btn-main ${buttonAVisible ? "visible" : "invisible"}`'>{{buttonAText}}</button>
+
+      <button @click="emitButtonB" :style="{backgroundColor: buttonBBgc, color: buttonBTextC}" :class='`btn-sub  ${buttonBVisible ? "visible" : "invisible"}`'>{{buttonBText}}</button>
     </div>
     </div>
   </div>
@@ -67,6 +68,15 @@ export default defineComponent({
             required:true,
             type: String
         },
+    },
+    emits:["buttonAEvent", "buttonBEvent"],
+    methods:{
+        emitButtonA(){
+            this.$emit('buttonAEvent', "emitted button A")
+        },
+        emitButtonB(){
+            this.$emit('buttonBEvent', "emitted button B")
+        }
     }
 })
 </script>
